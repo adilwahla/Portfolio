5ebaa5506c883c4f29cb9f8d2858e17750741606
Adil Rehman <adilwahla360@gmail.com>
2024-06-06 21:39:47 +0500

    done

---
+++ src/content_option.js
@@ -0,0 +1,189 @@
+import img_1 from '../src/assets/images/img_1.png'
+import img_2 from '../src/assets/images/img_2.jfif'
+import img_3 from '../src/assets/images/img_3.png'
+import img_4 from '../src/assets/images/img_4.png'
+import img_5 from '../src/assets/images/img_5.png'
+import img_6 from '../src/assets/images/img_6.png'
+import img_7 from '../src/assets/images/img_7.png'
+import img_8 from '../src/assets/images/img_8.png'
+import img_9 from '../src/assets/images/img_9.png'
+import img_10 from '../src/assets/images/img_10.jfif'
+import img_11 from '../src/assets/images/img_11.jpg'
+import img_12 from '../src/assets/images/img_12.png'
+
+const logotext = "Adil";
+const meta = {
+    title: "Adil Rehman",
+    description: "I’m Adil Rehman Full stack developer, currently working in KSA",
+};
+
+const introdata = {
+    title: "I’m Adil Rehman",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I develop  web & mobile apps",
+    },
+    description: "Experienced Software Engineer with over 5 years of expertise in full-stack development (MERN,Flutter,AI)",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "a bit about my self",
+    aboutme: `Experienced Software Engineer with over 5 years of expertise in full-stack web and app development 
+    actively pursuing opportunities in full-stack web or application development, `
+};
+const worktimeline = [{
+        jobtitle: "FrontEnd Developer",
+        where: "GAP Studio",
+        date: "2024",
+    },
+    {
+        jobtitle: "Full stack developer",
+        where: "Upwork",
+        date: "2022",
+    },
+    {
+        jobtitle: "Software Engineer",
+        where: "Islamabad",
+        date: "2020-2023",
+    },
+];
+
+const skills = [{
+        name: "Nodejs",
+        value: 90,
+    },
+    {
+        name: "MongoDB",
+        value: 85,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 60,
+    },
+    {
+        name: "RestFUL API",
+        value: 85,
+    },
+];
+
+const services = [{
+        title: "Full Stack Development",
+        description: "Full Stack Developer with 5+ years of experience in designing, developing, and deploying scalable web and mobile applications, integrating AI solutions. Proficient in MERN and Flutter stacks, with expertise in both front-end and back-end technologies, and a strong foundation in AI, CI/CD, Kubernetes, and AWS deployment.",
+    },
+    {
+        title: "Mobile Apps",
+        description: "Mobile Applications Developer specializing in cross-platform development with Flutter, bringing years of experience in building robust and intuitive applications for both Android and iOS platforms. Proficient in leveraging Flutter's capabilities to create seamless user experiences and optimize performance across diverse device types. Experienced in leading teams and managing end-to-end development cycles for mobile projects.",
+    },
+    {
+        title: " Shopify Wordpress Design",
+        description: "Shopify wordpress Developer with 100+ projects on upwork & Fiverr (Level-2 seller) with a proven track record of creating engaging and revenue-boosting online stores. Seeking an opportunity to leverage my in-depth knowledge of the Shopify platform, strong coding skills, and expertise in UI/UX design to contribute to a dynamic team focused on delivering exceptional e-commerce solutions.",
+    },
+];
+
+const dataportfolio = [{
+        img: img_1,
+        description: "Community Insights is AI Powered cockpit of Skool Community Management Tool That gives analytics That help Community managers to help them manage their Skool communities",
+        link: "https://communityinsights.io",
+    },
+    {
+        img: img_2,
+        description: "Operify uses open AI Api to generate email, translate documents, generate Reports and social media content it also allows to download pdf and text files. it works on organization level",
+        link: "https://operify.hu/",
+    },
+    {
+        img: img_3,
+        description: `
+        AiChat - AI powered full stack chat Application
+        Skills: Artificial Intelligence (AI) · Open ai · ChatGPT · React.js · MERN Stack  `,
+        link: "https://www.loom.com/share/7aede2a2f4924fc18a06a929107af091?t=40&sid=1b9586ef-df4b-4694-86b6-21ba04f917e9",
+    },
+    {
+        img: img_4,
+        description: `
+        SHOPPY- ECOM full stack web app
+        SHOPPY is a powerful and dynamic e-commerce web application built using the MERN stack (MongoDB, Express.js, React.js, Node.js). This application provides a seamless and engaging online shopping experience, leveraging modern technologies to deliver robust functionality, enhanced security, and an intuitive user interface.`,
+        link: "https://github.com/adilwahla/SHOPPY/tree/master",
+    },
+    {
+        img: img_5,
+        description: "Petticoat Lane is a premier Shopify e-commerce store specializing in a wide range of feminine apparel designed to cater to the modern woman's fashion needs. Our curated collection includes everything from elegant dresses and chic tops...",
+        link: "https://petticoatlane.com/",
+    },
+    {
+        img: img_6,
+        description: "This website, built on WordPress, is dedicated to scuba dive training courses. Whether you want to explore the depths of the sea or simply relax on a boat charter, Deep Blue Dive Training Center offers something for everyone.",
+        link: "https://deepbluedivetrainingcenter.com/",
+    },
+
+    {
+        img: img_7,
+        description: "Couture by Noor is a premium Shopify store where modesty meets modernity. ",
+        link: "https://couturebynoor.com",
+
+    },
+    {
+        img: img_8,
+        description: "DawaHazir - A complete medicine Delivery System  in Islamabad with Flutter, Dart, Firebase, and Laravel",
+        link: "https://www.youtube.com/watch?v=UyhefYRutCs&ab_channel=DawaHazir",
+        
+    },
+    {
+        img: img_9,
+        description: "Ecom store selling best Quality Saffron",
+        link: "https://www.zafferanoperlacucina.it/",
+    },
+    {
+        img: img_11,
+        description: `
+        FoodyOxFoodyOx:Hybrid Online Delivery System comprising web application and Android & iOS mobile applications for riders and customers. Led development, codebase management, and refactoring for enhanced performance.`,
+        link: "https://drive.google.com/file/d/1mY1v5xU1X7p9ZJ7y4A5DeEeWjkiOG0xs/view",
+    },
+    {
+        img: img_10,
+        description: `Defaang.io: Open-Source web application that curate recently asked interview questions from FAANG+ to help people practice & prep! by using Typescript, NextJS, Tailwind Css, Supabase and Vercel. Introduced new features compiler integration, discuss question`,
+        link: "https://github.com/adilwahla/defaang",
+     
+    },
+    {
+        img: img_12,
+        description: `social media threads clone appsocial media threads clone app
+        Skills: HTML5 · Vercel · Git · Web Development · Front-end Coding · MongoDB · Node.js · nodejs · JavaScript · Mongoose Web Server · GitHub · Application Programming Interfaces (API) · Full-Stack Development · Front-End Development · Express.js`,
+        link: "https://www.awesomescreenshot.com/video/20798891?key=1c07825b870565aabdf126f9c4abe5b7",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "adilwahla360@gmail.com",
+    YOUR_FONE: "+966509827158",
+    description: "Feel free to reach out to me via WhatsApp or give me a direct call at +966509827158. You can also contact me via email at adilrehman.158@outlook.com I'm available on both platforms and ready to chat about any opportunities or questions you may have. Looking forward to connecting with you!",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/adilwahla",
+    facebook: "https://facebook.com/adilrehman.wahla",
+    linkedin: "https://www.linkedin.com/in/adil-rehman158/",
+    twitter: "https://twitter.com/AdilWahla9",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
